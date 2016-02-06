ba1b0f8f95f11596caf0847f5058a6e242f6acaa
WoodMath <jeffwood314@gmail.com>
2016-02-05 23:23:26 -0600

    Generic changes

--- me/calib_images_v1/rename.sh
+++ me/calib_images_v1/rename.sh
@@ -13,13 +13,22 @@
 #		http://stackoverflow.com/questions/18460123/bash-for-loop-echo-leading-zeroi
 #		http://jonathanwagner.net/2007/04/zero-padding-in-bash/
 #		http://www.cyberciti.biz/tips/how-to-generating-print-range-sequence-of-numbers.html
+#		http://stackoverflow.com/questions/35231942/bash-shell-script-inner-loop-of-nested-loops-not-working?noredirect=1#comment58178785_35231942
 
-LIST=$(seq -f "%03g" 1 26)
-EXT="png"
-SIDE="right"
-for NUMBER in $LIST
+number_list=$(seq -f "%03g" 1 26)
+#LIST=(22,23,24,25,26)
+ext_list=("png" "bmp")
+side_list=("right" "left")
+for number in ${number_list[@]}
 do
-	OLD="image_${NUMBER}_${SIDE}.png"
-	NEW="${SIDE}_${NUMBER}.png"
-	mv $OLD $NEW
+	for side in ${side_list[@]}
+	do
+		for ext in ${ext_list[@]}
+		do	
+			old="${side}_${number}.${ext}";
+			new="${number}_${side}.${ext}";
+			mv $old $new;
+
+		done
+	done
 done
